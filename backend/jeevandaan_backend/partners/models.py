4e2432b09e83f03acc018b5e6442c98ebeb6e44f
Ujjawal Rauniyar <ujjawalrauniyar2004@gmail.com>
2026-03-20 12:56:14 +0530

    Partners model.py

--- backend/jeevandaan_backend/partners/models.py
+++ backend/jeevandaan_backend/partners/models.py
@@ -1,35 +1,35 @@
 from django.db import models
 
+class Partners(models.Model):
 
-class Donor(models.Model):
-    BLOOD_GROUPS = [
-        ('A+', 'A+'), ('A-', 'A-'),
-        ('B+', 'B+'), ('B-', 'B-'),
-        ('O+', 'O+'), ('O-', 'O-'),
-        ('AB+', 'AB+'), ('AB-', 'AB-'),
+    PARTNER_TYPES = [
+        ('government', 'Government'), ('private multi-specialty', 'Private Multi-Specialty'), ('blood-bank', 'BLOOD BANK')
     ]
 
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=128 , null=True,blank=True)
-    blood_group = models.CharField(max_length=3, choices=BLOOD_GROUPS) 
-    google_id = models.CharField(max_length=255 , unique=True , null=True,blank=True) # fixed: was blood_Group
-    auth_provider = models.CharField(max_length=50, choices=[('google', 'Google'), ('local', 'Local')], default='local')  # fixed: was auth_provider
-    def __str__(self):
-        return f"{self.name} ({self.blood_group})"
-
-
-class Partners(models.Model):
+    partner_type = models.CharField(max_length=50, choices=PARTNER_TYPES)
     hospital_name = models.CharField(max_length=100)   # fixed: was Hospital_name
     email = models.EmailField(unique=True)
     contact = models.CharField(max_length=15)          # fixed: was Contact
     address = models.TextField()
-    facility = models.TextField()                      # fixed: was Facility
-    license_id = models.CharField(max_length=50, unique=True)  # fixed: was L_id
+    partner_type = models.CharField(max_length=50, choices=PARTNER_TYPES)                      # fixed: was Facility
+    license_id = models.CharField(max_length=50, unique=True)
+    is_verified = models.BooleanField(default=False)
+    is_live = models.BooleanField(default=False)
+
+    convenience_fees = models.FloatField(max_digits=10, decimal_places=2, default=0.0)  # fixed: was convenience_fees
+    fees_description = models.TextField(blank=True, null=True) 
+    
+    
     password = models.CharField(max_length=128)
+
+     # Location (for map + nearest search)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.hospital_name
