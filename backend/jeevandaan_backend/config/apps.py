103362aaa7ab7bd911d6d7c685e0424038fcceb0
Ujjawal Rauniyar <ujjawalrauniyar2004@gmail.com>
2026-04-05 16:25:35 +0530

    here we go again

--- backend/jeevandaan_backend/config/apps.py
+++ backend/jeevandaan_backend/config/apps.py
@@ -1,10 +1,16 @@
 from django.apps import AppConfig
 
 
+import os
+
 class ConfigConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'config'
 
     def ready(self):
+        
+        if os.getenv('RENDER') == 'true':
+            return
+
         from scheduler import start
         start()
--- backend/jeevandaan_backend/config/settings.py
+++ backend/jeevandaan_backend/config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -94,17 +96,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': 'require',    # ← important!
-        },
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
