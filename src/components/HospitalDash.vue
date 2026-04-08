7c0bf5f841cba69c51a98a600d1c87a0c8463836
Ujjawal Rauniyar <ujjawalrauniyar2004@gmail.com>
2026-04-08 20:10:15 +0530

    header removed

--- src/components/HospitalDash.vue
+++ src/components/HospitalDash.vue
@@ -1164,11 +1164,8 @@ acceptingInterReq: null,
       this.loading = true
       this.error = null
       try {
-        const response = await api.get('https://jeevandaan-yaal.onrender.com/api/partners/profile/', {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  })
+        const response = await api.get('https://jeevandaan-yaal.onrender.com/api/partners/profile/', 
+  )
        
         this.partner = response.data
 
