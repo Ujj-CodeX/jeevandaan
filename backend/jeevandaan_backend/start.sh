b9fc2dff2fd9c85f44a442cd6f0c48ec3ea120ab
Ujjawal Rauniyar <ujjawalrauniyar2004@gmail.com>
2026-04-24 09:00:55 +0530

    Render Worker 1.4

--- backend/jeevandaan_backend/start.sh
+++ backend/jeevandaan_backend/start.sh
@@ -1,2 +1,2 @@
 python manage.py collectstatic --noinput --clear
-gunicorn config.wsgi --timeout 120 --workers 2 --worker-class sync --keepalive  5 --preload_app  True --max_requests  500 --max_requests_jitter  50
+gunicorn config.wsgi:application --workers 2 --timeout 120 --preload
